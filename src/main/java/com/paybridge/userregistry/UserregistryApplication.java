1493a603362ce91932ae5e3c9d84e9b77eef2b1b
Willy Catão <50681413+wscatao@users.noreply.github.com>
2024-11-17 15:47:56 -0300

    feat: Commit inicial

---
+++ src/main/java/com/paybridge/userregistry/UserregistryApplication.java
@@ -0,0 +1,13 @@
+package com.paybridge.userregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paybridge/userregistry/UserregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paybridge.userregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
